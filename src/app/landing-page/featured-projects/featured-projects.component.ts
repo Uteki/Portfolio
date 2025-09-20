d169d1c44db04a1c198e2b729d8805015f64c3e4
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-09-20 02:55:39 +0200

    Portfolio style non responsive & no dialog

--- src/app/landing-page/featured-projects/featured-projects.component.ts
+++ src/app/landing-page/featured-projects/featured-projects.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
 
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    NgSwitch,
+    NgClass,
+    NgForOf,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgIf
+  ],
   templateUrl: './featured-projects.component.html',
   styleUrl: './featured-projects.component.scss'
 })
 export class FeaturedProjectsComponent {
-
+  projects = [
+    {
+      name: 'Pok-Dex',
+      tech: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+      preview: 'pok-dex'
+    },
+    {
+      name: 'Join',
+      tech: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      preview: 'join'
+    },
+    {
+      name: 'Sharkie',
+      tech: ['HTML', 'CSS', 'JavaScript'],
+      preview: 'sharkie'
+    },
+    {
+      name: 'DA Bubble',
+      tech: ['Angular', 'TypeScript', 'Firebase'],
+      preview: 'da_bubble'
+    }
+  ];
 }
