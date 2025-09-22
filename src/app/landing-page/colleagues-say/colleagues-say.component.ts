f53eb94934546de07accf6aba9d67ea7efb5b1a0
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-09-22 03:36:43 +0200

    Colleagues say style non responsive

--- src/app/landing-page/colleagues-say/colleagues-say.component.ts
+++ src/app/landing-page/colleagues-say/colleagues-say.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-colleagues-say',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './colleagues-say.component.html',
-  styleUrl: './colleagues-say.component.scss'
+  styleUrls: ['./colleagues-say.component.scss'],
 })
-export class ColleaguesSayComponent {
+export class ColleaguesSayComponent implements AfterViewInit {
+  @ViewChild('swiper', { static: true }) swiperEl!: ElementRef;
 
+  ngAfterViewInit() {
+    const swiperContainer = this.swiperEl.nativeElement;
+    const swiperInstance = swiperContainer.swiper;
+
+    const prevBtn = document.querySelector('.prev') as HTMLElement;
+    const nextBtn = document.querySelector('.next') as HTMLElement;
+    const paginationContainer = document.querySelector('.custom-pagination') as HTMLElement;
+
+    const shadowRoot = this.swiperEl.nativeElement.shadowRoot;
+    const internalSlider = shadowRoot.querySelector('.swiper') as HTMLElement;
+
+    if (internalSlider) {
+      internalSlider.style.overflow = 'visible';
+    }
+
+    const updatePagination = () => {
+      if (!swiperInstance) return;
+
+      const realIndex = swiperInstance.realIndex;
+      const totalSlides = swiperInstance.slides.length;
+
+      paginationContainer.innerHTML = '';
+
+      for (let i = 0; i < totalSlides; i++) {
+        const bullet = document.createElement('div');
+        bullet.classList.add('bullet');
+        if (i === realIndex) bullet.classList.add('active');
+
+        bullet.addEventListener('click', () => swiperInstance.slideToLoop(i));
+        paginationContainer.appendChild(bullet);
+      }
+    };
+
+    updatePagination();
+    swiperInstance.on('slideChange', updatePagination);
+
+    prevBtn.addEventListener('click', () => swiperInstance.slidePrev());
+    nextBtn.addEventListener('click', () => swiperInstance.slideNext());
+  }
 }
