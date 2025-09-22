a2af23e1d0ca5f5d4f6607fd634fc9989334498f
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-09-23 00:29:19 +0200

    Contact style

--- src/app/landing-page/contact-me/contact-me.component.ts
+++ src/app/landing-page/contact-me/contact-me.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
+import {AnimationService} from "../../animation.service";
 
 @Component({
   selector: 'app-contact-me',
@@ -11,17 +12,38 @@ import {FormsModule, NgForm} from "@angular/forms";
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      console.log('Form submitted:', form.value);
-      //TODO
-    }
+
+  constructor(private animationService: AnimationService) {}
+
+  onHover(event: Event) {
+    const target = event.currentTarget as HTMLElement;
+    const targetBtn = event.target as HTMLButtonElement;
+
+    if (targetBtn.disabled) return;
+
+    this.animationService.startMarqueeBtnAnimation(target);
+  }
+
+  onHoverOut(event: Event) {
+    const target = event.currentTarget as HTMLElement;
+    const targetBtn = event.target as HTMLButtonElement;
+
+    if (targetBtn.disabled) return;
+
+    this.animationService.returnMarqueeBtnToCenter(target);
   }
 
   autoGrow(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     textarea.style.height = "0px";
     textarea.style.height = textarea.scrollHeight + "px";
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Form submitted:', form.value);
+      //TODO
+    }
   }
 
   hoverMiddle(isHovering: boolean) {
